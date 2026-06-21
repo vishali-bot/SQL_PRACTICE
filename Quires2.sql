f3a6bb8ff344a37a8368065567ef643181605832
VISHALI K <vishali67891@gmail.com>
2026-06-21 14:59:11 +0530

    new

--- Quires2.sql
+++ Quires2.sql
@@ -1,4 +1,4 @@
-/* Questions
+/* JOINS Questions
 
 1) Provide a table that provides the region for each sales_rep along with their associated accounts.
  This time only for the Midwest region. Your final table should include 
@@ -172,3 +172,80 @@ FROM (SELECT total_amt_usd
          LIMIT 3457) AS Table1
 ORDER BY total_amt_usd DESC
 LIMIT 2;
+
+
+/*GROUP BY Questions
+
+1)Which account (by name) placed the earliest order? 
+Your solution should have the account name and the date of the order.
+
+2)Find the total sales in usd for each account.
+ You should include two columns - the total sales for each company's orders in usd and the company name.
+
+3)Via what channel did the most recent (latest) web_event occur,
+ which account was associated with this web_event? 
+ Your query should return only three values - the date, channel, and account name.
+
+4)Find the total number of times each type of channel from the web_events was used. 
+Your final table should have two columns - the channel and the number of times the channel was used.
+
+5)Who was the primary contact associated with the earliest web_event?
+
+6)What was the smallest order placed by each account in terms of total usd. 
+Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+
+7)Find the number of sales reps in each region. 
+Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.*/
+
+
+1)SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY o.occurred_at
+LIMIT 1;
+
+
+2)SELECT a.name, SUM(total_amt_usd) AS Total_sales
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY a.name;
+
+
+3)SELECT  w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+
+4)SELECT channel, COUNT(*)
+FROM web_events
+GROUP BY channel;
+
+5)SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+6)SELECT a.name, MIN(total_amt_usd) smallest_order
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY smallest_order;
+
+
+7)SELECT r.name, COUNT(*) AS no
+FROM sales_reps s
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY no;
+
+
+
