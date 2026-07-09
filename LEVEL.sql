d2ef2fa850bd22b808747e6ce14edce0382ff9aa
VISHALI K <vishali67891@gmail.com>
2026-07-09 19:32:21 +0530

    new

--- LEVEL.sql
+++ LEVEL.sql
@@ -130,3 +130,98 @@ SELECT date_time, category,amount
 FROM expense_data WHERE income_expense ='Income'
 AND amount > (SELECT AVG(amount) AS avg_income FROM expense_data
 WHERE income_expense ='Income' ); 
+
+--Show all transactions whose amount is greater than the maximum expense amount.
+SELECT amount FROM expense_data 
+WHERE amount > 
+(SELECT MAX(amount)  FROM expense_data WHERE income_expense = 'Expense' 
+);
+
+--Show all expense transactions whose amount is equal to the highest expense
+SELECT SUBSTRING(date_time,1,7), amount ,category FROM expense_data
+WHERE amount =
+(SELECT MAX(amount) FROM expense_data WHERE income_expense ='Expense');
+
+
+--Write the SQL query to display:
+--| customer_name | order_id |
+| ------------- | -------: |
+| Alice         |      101 |
+| Alice         |      102 |
+| Bob           |      103 |
+
+SELECT c.customer_name, o.order_id
+FROM Customers c
+INNER JOIN Orders o
+ON c.customer_id = o.customer_id;
+
+--Display all customers who have NOT placed any orders.
+SELECT c.customer_name, o.order_id
+FROM Customers c
+LEFT JOIN Orders o
+ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
+
+--Write a query to display: Joining 3 Tables
+--customer_name	order_id	payment_mode
+SELECT c.customer_name,
+       o.order_id,
+       p.payment_mode
+FROM Customers c
+INNER JOIN Orders o
+ON c.customer_id = o.customer_id
+INNER JOIN Payments p
+ON o.order_id = p.order_id;
+
+--Write a query to display the total order amount for each customer.
+--| customer_name | total_amount |
+| ------------- | -----------: |
+| Alice         |          800 |
+| Bob           |          700 |
+
+SELECT
+    c.customer_name,
+    SUM(o.amount) AS total_amount
+FROM Orders o
+INNER JOIN Customers c
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_name;
+
+--Display every customer along with their total order amount.
+--COALESCE() to replace NULL with 0 so the report is easier to read
+SELECT
+    c.customer_name,
+    COALESCE(SUM(o.amount), 0) AS total_amount
+FROM Customers c
+LEFT JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_name;
+
+--Display the names of customers who have placed more than one order.
+SELECT c.customer_name
+FROM Customers c
+INNER JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_name
+HAVING COUNT(o.order_id) > 1;
+
+--Show the customer who spent the highest total amount.
+SELECT
+    c.customer_name,
+    SUM(o.amount) AS total_spent
+FROM Customers c
+INNER JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_name
+ORDER BY total_spent DESC
+LIMIT 1;
+
+--Show all customers whose total spending is greater than ₹600.
+SELECT
+    c.customer_name,
+    SUM(o.amount) AS total_spent
+FROM Customers c
+INNER JOIN Orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_name
+HAVING SUM(o.amount) > 600;
